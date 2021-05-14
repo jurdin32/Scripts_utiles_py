a28107efd616bcd48e8a92f69b15cdeaa8677b59
Johnny Urdin <urdin32@gmail.com>
2021-05-14 08:01:06 -0500

    Configuracion de email

--- enviar_email/configuracion.py
+++ enviar_email/configuracion.py
@@ -2,6 +2,6 @@ datos_email={
     'servidor_smtp':'smtp.gmail.com',
     'puerto':587,
     'use_tls':True,
-    'usuario':'xurimotos2021@gmail.com',
-    'password':'2021*Xurimotos**',
+    'usuario':'email@gmail.com',
+    'password':'password',
 }
