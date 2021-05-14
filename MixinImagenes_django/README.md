1fc34a3f97105421303877582a01b9248f27c455
Johnny Urdin <urdin32@gmail.com>
2021-05-13 22:18:34 -0500

    ResizeMixin

---
+++ MixinImagenes_django/README.md
@@ -0,0 +1,19 @@
+#ResizeImageMixin: es un modulo que sirve para redimensionar imagenes e incluir imagen como marca de agua directamente desde el modelo.
+
+
+Ejemplo:
+```
+class Imagenes(models.Model):
+    ......
+    foto=models.ImagenField(upload="fotos")
+
+    def(save):
+
+        self.resize(
+            imageField=self.foto, 
+            ancho=600, alto=600, 
+            format='png', 
+            marca=(350, 530)) # la marca es opcional
+```
+
+Es importante que se agregue las dimensiones, y el formato, la marca de agua es opcional
--- MixinImagenes_django/ResizeImageMixin.py
+++ MixinImagenes_django/ResizeImageMixin.py
@@ -7,18 +7,19 @@ from django.core.files.base import ContentFile
 
 class ResizeImageMixin:
     def resize(self, imageField, ancho, alto, format='JPEG', marca=()):
-        im = Image.open(imageField)  # Catch original
+        im = Image.open(imageField)  
         source_image = im.convert('RGBA')
-        source_image=source_image.resize((ancho,alto))  # Resize to size
+        source_image=source_image.resize((ancho,alto)) 
 
         if marca:
-            path=os.path.join(BASE_DIR, 'static/xurimotos.png')
+            path=os.path.join(BASE_DIR, 'static/xurimotos.png') #la marca de agua debe estar en una ruta por defecto
             imgAgua=Image.open(path)
             source_image.paste(imgAgua,marca,imgAgua)
+            
         output = BytesIO()
-        source_image.save(output, format=format, optimize=True, quality=90) # Save resize image to bytes
+        source_image.save(output, format=format, optimize=True, quality=90) 
         output.seek(0)
-        content_file = ContentFile(output.read())  # Read output and create ContentFile in memory
+        content_file = ContentFile(output.read())  
         file = File(content_file)
         random_name = f'{uuid.uuid4()}.jpeg'
         imageField.save(random_name, file, save=False)
