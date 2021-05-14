757cdf5398a256e1dad6770b1355c7c4219cd08c
Johnny Urdin <urdin32@gmail.com>
2021-05-14 08:23:22 -0500

    Generador Urls Parametros Get

---
+++ NavegacionActiva/GeneradorUrlsParametrosGet.js
@@ -0,0 +1,32 @@
+var QueryString = function () {
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+            query_string[pair[0]] = arr;
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+    }
+    return query_string;
+}();
+
+function url(nombre, valor) {
+    let uri = `${nombre}=${valor}`
+    for (const property in QueryString) {
+        if (property == nombre) {
+            console.log("existe")
+        } else {
+            if (QueryString[property] != "") {
+                uri += `&${property}=${QueryString[property]}`
+            }
+        }
+        console.log(uri)
+    }
+    location.href = "?" + uri+"#product"
+}
--- NavegacionActiva/README.md
+++ NavegacionActiva/README.md
@@ -5,4 +5,15 @@ Los enlaces deben tener como class='url' solo de esta manera el script sabra a c
 que apuntar cuando la url cambie.
 
 <a class='url' href='xxxxxx/xxxx'>
+
+```
+
+#GeneradorUrlsParametrosGet: Permite obtener los parametros get de la url.
+
+Ejemplo:
+```
+<a class="page-link" onclick="url('page',1)
+
+Esto deberia redireccionarlo a => www.pagina.com/?page=1
+
 ```
